c33cc8c6ab19a346386776c5db6b70c75d78f164
mouctar <mouctar.barry@outlook.com>
2025-04-25 23:47:31 +0200

    add user model test and update .gitignore for test environment

---
+++ test/userModel.test.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { User } from "../src/models/Users";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+beforeAll(async ()=>{
+    await mongoose.connect(process.env.MONGODB_URI || '');
+});
+
+afterAll(async ()=>{
+    await mongoose.connection.dropDatabase()
+    await mongoose.disconnect();
+});
+
+describe('User Model Test', ()=>{
+    it('should create & save a user successfully', async () => {
+        const user = new User({
+            name: 'Mouctar',
+            email: 'mouctar@caramail.fr',
+            password: "secured-password-1020"
+        })
+
+        const savedUser = await user.save();
+
+        expect(savedUser._id).toBeDefined();
+        expect(savedUser.email).toBe("Mouctar");
+        expect(savedUser.email).toBe("mouctar@caramail.fr");
+        expect(savedUser.password).toBe("secured-password-1020");
+    });
+})
+
+
