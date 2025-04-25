42a7811c1cd8125f953521b5f74e9668721419ce
mouctar <mouctar.barry@outlook.com>
2025-04-25 18:39:45 +0200

    initialize Express server with MongoDB connection and basic API endpoint

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || '';
+
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log('✅ MongoDB connecté avec succès');
+        app.listen(PORT, () => {
+            console.log(`🚀 Serveur en écoute sur http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('❌ Erreur de connexion MongoDB :', err.message);
+    });
+
+app.get('/', (_, res) => {
+    res.send('Bienvenue sur l’API TypeScript MongoDB 🎉');
+});
